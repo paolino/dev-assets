1bc3455e02b11ef37c6e7d40fb0a00a017090eff
paolino <paolo.veronelli@gmail.com>
2025-11-27 11:55:22 +0000

    feat: simplify packaging

--- mkdocs/flake.nix
+++ mkdocs/flake.nix
@@ -24,21 +24,20 @@
       perSystem =
         { system, pkgs, ... }:
         let
-          asciinema-plugin = import ./asciinema-plugin.nix { inherit pkgs; };
-          markdown-callouts-plugin = import ./markdown-callouts-plugin.nix { inherit pkgs; };
-          mkdocs-packages = pkgs.python3.withPackages (ps: [ ps.mkdocs-material ]);
+          plugins = {
+            asciinema-plugin = pkgs.callPackage ./nix/asciinema-plugin.nix { };
+            markdown-callouts = pkgs.callPackage ./nix/markdown-callouts.nix { };
+            from-nixpkgs = pkgs.python3.withPackages (ps: [
+              ps.mkdocs-material
+            ]);
+          };
         in
-        {
-          packages.mkdocs-asciinema-player = asciinema-plugin;
-          packages.mkdocs-markdown-callouts = markdown-callouts-plugin;
-          packages.mkdocs-packages = mkdocs-packages;
+        rec {
+          packages = plugins;
           devShells.default = pkgs.mkShell {
             packages = [
-              asciinema-plugin
-              markdown-callouts-plugin
               pkgs.mkdocs
-              mkdocs-packages
-            ];
+            ] ++ (builtins.attrValues plugins);
 
             shellHook = ''
               echo "MkDocs environment ready!"
